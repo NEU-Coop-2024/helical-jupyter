4c0351a3890bb7c2856da9cf3ab4c6014e34823e
Kevin Yang <kevingyang72@gmail.com>
2024-07-23 17:21:40 -0400

    edits to toml file

---
+++ echo_append/.ipynb_checkpoints/echo_append-checkpoint.py
@@ -0,0 +1,23 @@
+from IPython.core.magic import Magics, magics_class, cell_magic
+from IPython import get_ipython
+
+
+@magics_class
+class EchoMagics(Magics):
+    def __init__(self, *args, **kwargs):
+        self.echo_string = ""
+        super().__init__(*args, **kwargs)
+
+    @cell_magic
+    def echo_append(self, line, cell):
+        self.echo_string += cell + '\n'
+        return self.echo_string + '\nprint("Success!")'
+
+
+def load_ipython_extension(ipython):
+    ipython.register_magics(EchoMagics)
+
+
+
+
+
