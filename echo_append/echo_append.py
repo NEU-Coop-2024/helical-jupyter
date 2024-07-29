f6cf59c3bcedef116969c58f02929fe36123dfba
Kevin Yang <kevingyang72@gmail.com>
2024-07-29 13:32:56 -0400

    process DOT code

--- echo_append/echo_append.py
+++ echo_append/echo_append.py
@@ -1,5 +1,8 @@
 from IPython.core.magic import Magics, magics_class, cell_magic
 from IPython import get_ipython
+import graphviz
+import datetime
+from IPython.display import Image, display
 
 
 @magics_class
@@ -10,8 +13,13 @@ class EchoMagics(Magics):
 
     @cell_magic
     def echo_append(self, line, cell):
-        self.echo_string += cell + '\n'
-        exec(self.echo_string + '\nprint("Success!")')
+
+        dot = graphviz.Source(cell)
+        output_filename = dot.render(filename=f'graph_{datetime.datetime.now().strftime("%Y%m%d_%H%M%S")}', format='png')
+        
+        display(Image(filename=output_filename))
+        print("Success!")
+        
 
 
 def load_ipython_extension(ipython):
